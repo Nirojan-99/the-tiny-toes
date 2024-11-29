d1ad57d42689afef319d5a66d03bd73b4f4d5f0c
nro <nirojan.yoga@gmail.com>
2024-11-29 11:43:50 +0530

    #3.1 Storage service implemented

---
+++ client/apps/baby_gallery/lib/services/storage_service.dart
@@ -0,0 +1,15 @@
+import 'package:shared_preferences/shared_preferences.dart';
+class StorageService {
+  Future<void> storeUsername(String username) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('username', username);
+  }
+  Future<String?> getUsername() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('username');
+  }
+  Future<void> clearStorage() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
+  }
+}
