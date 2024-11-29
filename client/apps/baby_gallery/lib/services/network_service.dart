ccb2c6d085993355d2ff6a124992162b7a197ff3
nro <nirojan.yoga@gmail.com>
2024-11-29 11:45:16 +0530

    #3.2 network service implemented

---
+++ client/apps/baby_gallery/lib/services/network_service.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class NetworkService {
+  Future<dynamic> fetchData(String url) async {
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      } else {
+        throw Exception('Failed to fetch data');
+      }
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
+  }
+}
